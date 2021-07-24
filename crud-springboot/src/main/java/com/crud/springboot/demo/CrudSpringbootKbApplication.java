8c5ff189d983c8d63e3d1321ccbecca36677e687
kabita bhandari <36861110+kabitabhandari@users.noreply.github.com>
2021-07-24 15:52:48 -0500

    chore: empty project

---
+++ crud-springboot/src/main/java/com/crud/springboot/demo/CrudSpringbootKbApplication.java
@@ -0,0 +1,13 @@
+package com.crud.springboot.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudSpringbootKbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSpringbootKbApplication.class, args);
+	}
+
+}
---
+++ crud-springboot/src/test/java/com/crud/springboot/demo/CrudSpringbootKbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.springboot.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSpringbootKbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
