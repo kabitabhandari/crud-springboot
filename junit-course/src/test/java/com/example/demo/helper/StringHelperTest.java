4c815415a00cebf075fdaf54594450a710297dc4
kabita bhandari <36861110+kabitabhandari@users.noreply.github.com>
2021-08-01 19:04:12 -0500

    chore: basic test case

--- junit-course/src/test/java/com/example/demo/helper/StringHelperTest.java
+++ junit-course/src/test/java/com/example/demo/helper/StringHelperTest.java
@@ -1,4 +1,40 @@
 package com.example.demo.helper;
 
+import com.example.demo.StringHelper;
+import org.junit.Assert;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class StringHelperTest {
+
+    StringHelper sh = new StringHelper();
+    @Test
+    public void shouldtruncateAInFirst2Positions(){
+         final String name = "AAple";
+        String actualValue = sh.truncateAInFirst2Positions(name);
+        Assert.assertEquals( "ple", actualValue);
+    }
+
+    @Test
+    public void shouldTruncatetoEmptyStringWhenOnlyTwoCharPresentAndBothAs(){
+        final String name = "AA";
+        String actualValue = sh.truncateAInFirst2Positions(name);
+        Assert.assertEquals("", actualValue);
+    }
+
+    @Test
+    public void firstAndLastTwoCharactersTheSameShouldReturnTrue(){
+        final String name = "AAAA";
+        Boolean actualValue = sh.areFirstAndLastTwoCharactersTheSame(name);
+        Assert.assertTrue(actualValue);
+    }
+
+    @Test
+    public void firstAndLastTwoCharactersTheSameShouldReturnFalse(){
+        final String name = "AABB";
+        Boolean actualValue = sh.areFirstAndLastTwoCharactersTheSame(name);
+        Assert.assertFalse(actualValue);
+    }
+
+
 }
